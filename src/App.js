bcbc5cc7054eda72726da36f78f27b93f7e10d54
mochtaufiqidayat <mochammadtaufiq779@gmail.com>
2023-08-04 08:23:38 +0700

    add login pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import logo from './logo.svg';
+import Login from '../src/features/pages/Login'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          First
-        </a>
-      </header>
+     <Login />
     </div>
   );
 }
---
+++ src/features/pages/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsloggedIn] = useState(false);
+
+  const handleLogin = () => {
+    const exampleEmail = "user@gmail.com"
+    const examplePassword = "123"
+
+    if (email === exampleEmail && password === examplePassword) {
+      setIsloggedIn(true)
+      alert('Login succes')
+    } else {
+      alert('Invalid email or password')
+    }
+  }
+
+  const handleLogout = () => {
+    setIsloggedIn(false)
+  }
+  return (
+    <>
+      <div>
+        {isLoggedIn ? (
+          <div>
+            <h1>Welcome {email}</h1>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <div>
+            <input 
+              type='email'
+              placeholder='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <input 
+              type='password'
+              placeholder='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <button onClick={handleLogin}>Login</button>
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Login
