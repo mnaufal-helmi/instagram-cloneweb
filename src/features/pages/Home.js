5889c667559c7516364ac5978559793804b24a74
mnaufalhelmi <mnaufalhelmi@gmail.com>
2023-08-08 21:20:35 +0700

    add SidebarNav

--- src/features/pages/Home.js
+++ src/features/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import SidebarNav from '../Layouts/SidebarNav'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <SidebarNav/>
+    </div>
   )
 }
 
