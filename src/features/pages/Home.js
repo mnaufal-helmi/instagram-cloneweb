f5a7b46e5597efe8c7709d0434d3b1891cb18bbf
mnaufalhelmi <mnaufalhelmi@gmail.com>
2023-10-04 12:19:22 +0700

    update footbar

--- src/features/pages/Home.js
+++ src/features/pages/Home.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import SidebarNav from '../Layouts/SidebarNav'
+import React from 'react';
+import SidebarNav from '../Layouts/SidebarNav';
+import Footbar from '../Layouts/Footbar';
 
 function Home() {
   return (
-    <div>
-      <SidebarNav/>
+    <div style={{ flexDirection: 'row' }}>
+      <div>
+        <SidebarNav />
+      </div>
+      <div>
+        <Footbar />
+      </div>
     </div>
-  )
+  );
 }
 export default Home;
-
